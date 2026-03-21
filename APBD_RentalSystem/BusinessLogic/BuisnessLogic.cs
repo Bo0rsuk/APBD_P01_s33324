751579ac0f0b42c5f2d3ac14240b832dbbd2e041
Stanisław Wołek <s33324@pjwstk.edu.pl>
2026-03-21 23:08:28 +0100

    Added empty BuisnessLogic class with interface (loose coupling. Started work on DataProcessing which handles rental logging (may be thought of as CRUD behaviour)

---
+++ APBD_RentalSystem/BusinessLogic/BuisnessLogic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic
+{
+    public class BuisnessLogic : IBuisnessLogic
+    {
+    }
+}
--- APBD_RentalSystem/DataProcessing/DataRepository.cs
+++ APBD_RentalSystem/DataProcessing/DataRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DataModels;
+using BusinessLogic;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,10 +11,13 @@ namespace DataProcessing
     {
         private List<Person> persons = new List<Person>();
         private List<Equipment> equipments = new List<Equipment>();
-        
+        private List<RentalCard> rentalCards = new List<RentalCard>();
 
+        private IBuisnessLogic buisnessLogic;
 
-
-        
+        public DataRepository(IBuisnessLogic buisnessLogic)
+        {
+            this.buisnessLogic = buisnessLogic;
+        }
     }
 }
